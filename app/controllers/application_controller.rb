5d8928df485e138b9f7b309f265699f439655569
Nick <nemtiax@brandeis.edu>
2014-12-05 17:24:00 -0500

    Changed reverse.first to last for tweet queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,15 @@ class ApplicationController < ActionController::Base
 	
 
 	def get_followed_tweets(user, num_results)
-		user.followed_tweets.order(:created_at).reverse.first(num_results)
+		user.followed_tweets.order(:created_at).last(num_results)
 	end
 	
 	def get_users_tweets(user,num_results)
-		user.tweets.order(:created_at).reverse.first(num_results)
+		user.tweets.order(:created_at).last(num_results)
 	end
 	
 	def get_recent_tweets(num_results)
-		Tweet.all.order(:created_at).reverse.first(num_results)
+		Tweet.all.order(:created_at).last(num_results)
 	end
 
 
