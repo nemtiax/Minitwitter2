904e306d70f4b4c33dc1bd18300aca7e55b95fc6
Nick <nemtiax@brandeis.edu>
2014-12-28 20:27:34 -0500

    unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,13 +3,24 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 preload_app true
 # ...
 before_fork do |server, worker|
-  # ...
-  defined?(ActiveRecord::Base) and
+  # other settings
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
+  end
 end
 
 after_fork do |server, worker|
-  # ...
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  # other settings
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool']            =   ENV['DB_POOL'] || 2
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
+
+
+
+
+timeout 15
+
